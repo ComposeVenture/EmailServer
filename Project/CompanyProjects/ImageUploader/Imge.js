c9b709913939b17263ff8c2738a43ca8adb0cec3
ComposeVenture <composeventures@gmail.com>
2024-10-20 09:18:43 +0300

    luna

---
+++ Project/CompanyProjects/ImageUploader/Imge.js
@@ -0,0 +1,25 @@
+const AUTORUN=()=>{
+
+    HOMEPAGE();
+
+};
+
+const HOMEPAGE=()=>{
+
+    DISPLAY('',`
+
+        <div class='HomeDiv'>
+
+            <p>No Images Saved</p>
+        
+        </div>
+
+        <button class='FloatButton'>
+
+            <img src='${WHITEPOSTICON}'/>
+        
+        </button>
+        
+    `);
+
+}
